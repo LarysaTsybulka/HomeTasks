6e7164c94589b44a4c1934fbe59dd302d284f454
Larysa Tsybulka <larri1992@gmail.com>
2020-03-25 18:14:26 +0200

    Minor improvements for AverageLength

--- MainTask/src/AverageLength.java
+++ MainTask/src/AverageLength.java
@@ -4,7 +4,7 @@ public class AverageLength {
     private static final String TEXT_ENTER_NUMBER = "Введите числа через пробел: ";
     private static final String TEXT_AVERAGE_LENGTH = "Средняя длина по всем числам  равна: %d %n";
     private static final String SHOW_NUMBER_LESS_LENGTH = "Числа, длина которых меньше средней:";
-    private static final String SHOW_NUMBER_MORE_LENGTH = "Числа, длина которых больше средней:";
+    private static final String SHOW_NUMBER_MORE_LENGTH = "Числа, длина которых больше либо равна средней:";
     private static final String SHOW_NUMBER = "Число: %s, его длина: %d %n";
 
     public static void main(String[] args) {
@@ -13,33 +13,37 @@ public class AverageLength {
         String stringNumbers = in.nextLine();
         String[] numbers = stringNumbers.split(" ");
         in.close();
-        int average = findAverage(numbers);
-        System.out.printf(TEXT_AVERAGE_LENGTH, average);
+        int avgLenght = findAverage(numbers);
+
+
+        System.out.printf(TEXT_AVERAGE_LENGTH, avgLenght);
         System.out.println(SHOW_NUMBER_LESS_LENGTH);
-        showLessAvg(numbers, average);
+
+        showLessAvg(numbers, avgLenght);
         System.out.println(SHOW_NUMBER_MORE_LENGTH);
-        showMoreAvg(numbers, average);
+        showMoreAvg(numbers, avgLenght);
     }
 
     private static int findAverage(String[] numbers) {
-        int avg = 0;
-        for (String num : numbers) avg += num.length();
-        return avg = (int) (avg / numbers.length);
+        int commonLenght = 0;
+        for (String num : numbers) {
+            commonLenght += num.length();
+        }
+        float avgLenght = commonLenght / (float) numbers.length;
+        return (int) ((avgLenght - (int) avgLenght) >= 0.5f ? avgLenght + 1.0f : avgLenght);
     }
 
-    private static void showLessAvg(String[] numbers, int average) {
-        int avg = average;
+    private static void showLessAvg(String[] numbers, final int avgLenght) {
         for (String num : numbers) {
-            if (num.length() < avg) {
+            if (num.length() < avgLenght) {
                 System.out.printf(SHOW_NUMBER, num, num.length());
             }
         }
     }
 
-    private static void showMoreAvg(String[] numbers, int average) {
-        int avg = average;
+    private static void showMoreAvg(String[] numbers, final int avgLenght) {
         for (String num : numbers) {
-            if (num.length() > avg) {
+            if (num.length() >= avgLenght) {
                 System.out.printf(SHOW_NUMBER, num, num.length());
             }
         }
